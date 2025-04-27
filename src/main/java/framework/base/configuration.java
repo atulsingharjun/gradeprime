439807d5a9636895fd8380749be9c66645cd331a
Atul <atulksingh.15@gmail.com>
2025-04-27 20:17:49 +0530

    27 apr

--- src/main/java/framework/base/configuration.java
+++ src/main/java/framework/base/configuration.java
@@ -1,25 +1,32 @@
 package framework.base;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
-public class configuration {
-	public 	 String link ="https://gradeprime.simplyone.ai/#";
-	public WebDriver driver;
-	@BeforeTest
-	public void setup() {
-		
-		driver =new ChromeDriver();
-		
-	}
-	@AfterTest
-	public void teardown() {
-		if(driver!=null) {
-			driver.quit();
-		}
-		
-	}
 
+public class configuration {
+   public String link = "https://gradeprime.simplyone.ai/#";  
+   public WebDriver driver;
+    
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+        
+       
+    }
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    
+    // Capture screenshot
+   
 }
--- src/test/java/gradeprime/login.java
+++ src/test/java/gradeprime/login.java
@@ -1,18 +1,34 @@
 package gradeprime;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import framework.base.configuration;
+import framework.utils.failcases;
 
+@Listeners(failcases.class)
 public class login extends configuration  {
 	
-	  //configuration config = new configuration();  if we can not extend then we use this
+	    public WebDriver driver;
+	    
+	    @BeforeTest
+	    public void setup() {
+	        driver = new ChromeDriver();
+	        
+	       
+	    }
 	 
-	@Test
+	@Test(testName="validlogin")
 	public void validlogin() {
 		 
 		 driver.get(link);
 	     System.out.println("Title is: " + driver.getTitle());
-		
+	   
+	     driver.findElement(By.xpath(".."));
+	    
 	}
 
 }
